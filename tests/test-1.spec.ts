dc61637f8eb4f2d5f4568df0888f540b31d5f2ec
Divya Patidar <divya.patidar@valtech.com>
2026-03-20 17:47:01 +0530

    Add Playwright GitHub Actions workflow

--- tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://workspace.google.com/gmail/');
-  await page.getByTestId('Create an account').getByText('Create an account').click();
-  await page.getByRole('menuitem', { name: 'Open the Gmail - For my personal use page in a new tab' }).click();
-});
